01e4730ce7e34080ca0ae440c4a16c4b88343140
Anton Kica <kica6@uniba.sk>
2023-01-04 15:08:06 +0100

    Created hand drawing iintegration tests

---
+++ src/test/game/handdrawing/HandDrawingTests.java
@@ -0,0 +1,18 @@
+package game.handdrawing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HandDrawingTests {
+
+	game.player.Hand hand;
+
+	@Before
+	void init() {
+
+	}
+	@Test
+	public void hand_drew_correct_ammount() {
+
+	}
+}
